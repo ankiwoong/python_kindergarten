e5193d321897f6adf1ebadc018b3b29aacfdf5bb
AnKiWoong <ankiwoong@gmail.com>
2020-04-26 11:02:17 +0900

    Modify urls.py(Blog URL & Basic URL handling)

--- anjia/urls.py
+++ anjia/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    # 주소창에 blog를 입력시 blog.urls가 처리
+    path('blog/', include('blog.urls')),
+    # 기본주소로 처리시 blog로 넘겨준다.
+    path('', RedirectView.as_view(url='/blog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)     # 정적 파일 처리
