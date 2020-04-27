dbad39374a276697bad1b15f2b026e4ae4f83f2b
ankiwoong <ankiwoong@gmail.com>
2020-04-27 12:29:51 +0900

    Post Modify

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
     # 수정일 : 사용자가 입력하지 않고 현재 날짜를 받아옴
     updateDate = models.DateTimeField(auto_now_add=True)
     # 분류 : 다대다 관계정의
-    category = models.ManyToManyField(category, help_text='분류를 입력하세요.')
+    category = models.ManyToManyField(Category, help_text='분류를 입력하세요.')
 
     def __str__(self):
         return self.title
@@ -35,4 +35,4 @@ class Post(models.Model):
     # 1번 글의 경우 -> single/1
 
     def get_absolute_url(self):
-        return reverse('single'. args=[str(self.id)])
+        return reverse('single', args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.models import Category, Post
 
 # Create your views here.
 
@@ -6,8 +7,9 @@ from django.shortcuts import render
 
 
 def index(req):
+    post_latest = Post.objects.order_by("-createDate")[:6]
     context = {
-
+        "post_latest": post_latest
     }
 
     return render(req, 'index.html', context=context)
