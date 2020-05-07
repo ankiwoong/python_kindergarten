4261367349cfb68856c6f1d1120ab65ba8e62f63
AnKiWoong <ankiwoong@gmail.com>
2020-05-07 21:11:51 +0900

    Admin Page Search ADD

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from blog.models import Category, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
