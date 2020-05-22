c7a59927c40472739aa37844acc330c97259e0bb
AnKiWoong <ankiwoong@gmail.com>
2020-05-22 21:13:43 +0900

    HTTP error code handling

--- anjia/urls.py
+++ anjia/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from django.conf.urls import handler400, handler500
+from django.conf.urls import handler400, handler403, handler404, handler500
 
 
 # http://localhost:8000/ => http://localhost:8000/blog
@@ -38,5 +38,7 @@ urlpatterns += static(settings.STATIC_URL,
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)     # media 정적 파일 처리
 
+handler400 = 'blog.views.error400'
+handler403 = 'blog.views.error403'
 handler404 = 'blog.views.error404'
 handler500 = 'blog.views.error500'
--- blog/views.py
+++ blog/views.py
@@ -51,9 +51,17 @@ def post_video(req):
     return render(req, 'blog/post_video.html')
 
 
-def error404(request, exception):
-    return render(request, "404.html", status=404)
+def error400(req, exception):
+    return render(req, "errors/400.html", status=400)
 
 
-def error500(request):
-    return render(request, "500.html", status=500)
+def error403(req, exception):
+    return render(req, "errors/403.html", status=403)
+
+
+def error404(req, exception):
+    return render(req, "errors/404.html", status=404)
+
+
+def error500(req):
+    return render(req, "errors/500.html", status=500)
